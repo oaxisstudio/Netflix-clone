d850b47d6688ef02402d43f23553172594ce2c35
kaoru-t <oaxisstudio@gmail.com>
2022-12-24 08:00:33 +0900

    Bannerコンポーネント, Navコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import "./App.css";
 import { requests } from "./request";
 import { Row } from "./components/Row";
+import { Banner } from "./components/Banner";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Banner />
       <Row title="NETFLIX ORIGUINALS" fetchUrl={requests.feachNetflixOriginals} isLargeRow />
       <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
-      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+      <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
